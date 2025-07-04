2f46c587d6a0f6268fa78ac2382b281d7d7af214
Lovely Clareon <142940454+clrnlvly@users.noreply.github.com>
2025-07-04 18:01:02 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,29 @@
 /**Resume section tabs and tab content */
-const resumeTab = document.querySelector(".resume-tabs");
+const resumeTabs = document.querySelector(".resume-tabs");
+const resumePortfolioTabBtns = resumeTabs.querySelectorAll(".tab-btn");
+const resumeTabContents = document.querySelectorAll(".resume-tab-content");
 
-2:10:11
+var resumeTabNav = function(resumeTabClick){
+    resumeTabContents.forEach((resumeTabContent) => {
+        resumeTabContent.style.display = "none";
+        resumeTabContent.classList.remove("active");
+    });
+
+    resumePortfolioTabBtns.forEach((resumePortfolioTabBtn) => {
+        resumePortfolioTabBtn.classList.remove("active");
+    });
+
+
+    resumeTabContents[resumeTabClick].style.display = "flex";
+    setTimeout(() => {
+        resumeTabContents[resumeTabClick].classList.add("active");
+    }, 100);
+
+    resumePortfolioTabBtns[resumeTabClick].classList.add("active");
+}   
+
+resumePortfolioTabBtns.forEach((resumePortfolioTabBtns, i) => {
+    resumePortfolioTabBtns.addEventListener("click", () => {
+        resumeTabNav(i);
+    });
+});
