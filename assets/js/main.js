cd5e5aff69321d535b8356576b93d8dcdd6b3ce2
clrnlvly <142940454+clrnlvly@users.noreply.github.com>
2025-07-08 11:07:58 +0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,72 +1,72 @@
 /**Resume section tabs and tab content */
-// const resumeTabs = document.querySelector(".resume-tabs");
-// const resumePortfolioTabBtns = resumeTabs.querySelectorAll(".tab-btn");
-// const resumeTabContents = document.querySelectorAll(".resume-tab-content");
+const resumeTabs = document.querySelector(".resume-tabs");
+const resumePortfolioTabBtns = resumeTabs.querySelectorAll(".tab-btn");
+const resumeTabContents = document.querySelectorAll(".resume-tab-content");
 
-// var resumeTabNav = function(resumeTabClick){
-//     resumeTabContents.forEach((resumeTabContent) => {
-//         resumeTabContent.style.display = "none";
-//         resumeTabContent.classList.remove("active");
-//     });
+var resumeTabNav = function(resumeTabClick){
+    resumeTabContents.forEach((resumeTabContent) => {
+        resumeTabContent.style.display = "none";
+        resumeTabContent.classList.remove("active");
+    });
 
-//     resumePortfolioTabBtns.forEach((resumePortfolioTabBtn) => {
-//         resumePortfolioTabBtn.classList.remove("active");
-//     });
+    resumePortfolioTabBtns.forEach((resumePortfolioTabBtn) => {
+        resumePortfolioTabBtn.classList.remove("active");
+    });
 
 
-//     resumeTabContents[resumeTabClick].style.display = "flex";
-//     setTimeout(() => {
-//         resumeTabContents[resumeTabClick].classList.add("active");
-//     }, 100);
+    resumeTabContents[resumeTabClick].style.display = "flex";
+    setTimeout(() => {
+        resumeTabContents[resumeTabClick].classList.add("active");
+    }, 100);
 
-//     resumePortfolioTabBtns[resumeTabClick].classList.add("active");
-// }   
+    resumePortfolioTabBtns[resumeTabClick].classList.add("active");
+}   
 
-// resumePortfolioTabBtns.forEach((resumePortfolioTabBtns, i) => {
-//     resumePortfolioTabBtns.addEventListener("click", () => {
-//         resumeTabNav(i);
-//     });
-// });
+resumePortfolioTabBtns.forEach((resumePortfolioTabBtns, i) => {
+    resumePortfolioTabBtns.addEventListener("click", () => {
+        resumeTabNav(i);
+    });
+});
 
 
 /**Services - Modal */
 
-// const serviceCardWithModals = document.querySelectorAll(".services-container .card-with-modal");
+const serviceCardWithModals = document.querySelectorAll(".services-container .card-with-modal");
 
-// serviceCardWithModals.forEach((serviceCardWithModal) => {
-//     const serviceCard = serviceCardWithModal.querySelector(".service-card");
-//     const serviceBackdrop = serviceCardWithModal.querySelector(".service-modal-backdrop");
-//     const serviceModal = serviceCardWithModal.querySelector(".service-modal");
-//     const modalCloseBtn = serviceCardWithModal.querySelector(".modal-close-btn");
+serviceCardWithModals.forEach((serviceCardWithModal) => {
+    const serviceCard = serviceCardWithModal.querySelector(".service-card");
+    const serviceBackdrop = serviceCardWithModal.querySelector(".service-modal-backdrop");
+    const serviceModal = serviceCardWithModal.querySelector(".service-modal");
+    const modalCloseBtn = serviceCardWithModal.querySelector(".modal-close-btn");
 
-//     serviceCard.addEventListener("click", () => {
-//         serviceBackdrop.style.display = "flex";
+    serviceCard.addEventListener("click", () => {
+        serviceBackdrop.style.display = "flex";
 
-//         setTimeout(() => {
-//             serviceBackdrop.classList.add("active");
-//         }, 100);
+        setTimeout(() => {
+            serviceBackdrop.classList.add("active");
+        }, 100);
 
-//         setTimeout(() => {
-//             serviceModal.classList.add("active");
-//         }, 300);
+        setTimeout(() => {
+            serviceModal.classList.add("active");
+        }, 300);
                 
-//     });
+    });
 
-//     modalCloseBtn.addEventListener("click", () => {
+    modalCloseBtn.addEventListener("click", () => {
 
 
-//         setTimeout(() => {
-//             serviceBackdrop.style.display = "none";
-//         }, 500);
+        setTimeout(() => {
+            serviceBackdrop.style.display = "none";
+        }, 500);
 
         
-//         setTimeout(() => {
-//             serviceBackdrop.classList.remove("active");
-//             serviceModal.classList.remove("active");
-//         }, 100);
-//     });
+        setTimeout(() => {
+            serviceBackdrop.classList.remove("active");
+            serviceModal.classList.remove("active");
+        }, 100);
+    });
 
-// });
+});
 
 /**Portfolio */
 document.addEventListener("DOMContentLoaded", () => {
@@ -174,3 +174,5 @@ lvContactForm.addEventListener('submit', function(event) {
             }, 5000);
         });
 });
+
+/**Header on Scroll */
